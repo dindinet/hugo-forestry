81e0126d8990d0ae231738d7b855f3b5065dc0a4
dindinet <github.dklongley@neverbox.com>
2016-03-27 15:22:38 +0100

    add slideshow click handler to scripts js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -283,3 +283,8 @@ jQuery(document).ready(function(){
     initializeJS();
     $('[rel=show-github]').showGithub();
 });
+$('#slidemenu a').on("click",function(){
+var clicker = $(this).attr("slide");
+$('#slides div').addClass('hide');
+$('#'+clicker).addClass('show')
+});
